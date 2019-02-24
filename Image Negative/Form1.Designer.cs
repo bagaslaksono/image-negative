a718211a2c35fe7e6b885bd956891426f1d1576d
bagaslaksono <bagas190598@gmail.com>
2019-02-24 11:53:52 +0700

    image negative

---
+++ Image Negative/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Image_Negative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Open = new System.Windows.Forms.Button();
+            this.pb_1 = new System.Windows.Forms.PictureBox();
+            this.pb_result = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_negative = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(782, 35);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(75, 23);
+            this.btn_Open.TabIndex = 0;
+            this.btn_Open.Text = "Open Image";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // pb_1
+            // 
+            this.pb_1.Location = new System.Drawing.Point(12, 12);
+            this.pb_1.Name = "pb_1";
+            this.pb_1.Size = new System.Drawing.Size(346, 426);
+            this.pb_1.TabIndex = 1;
+            this.pb_1.TabStop = false;
+            // 
+            // pb_result
+            // 
+            this.pb_result.Location = new System.Drawing.Point(395, 12);
+            this.pb_result.Name = "pb_result";
+            this.pb_result.Size = new System.Drawing.Size(346, 426);
+            this.pb_result.TabIndex = 2;
+            this.pb_result.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 457);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Image 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(555, 457);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Image Result";
+            // 
+            // btn_negative
+            // 
+            this.btn_negative.Location = new System.Drawing.Point(782, 116);
+            this.btn_negative.Name = "btn_negative";
+            this.btn_negative.Size = new System.Drawing.Size(75, 23);
+            this.btn_negative.TabIndex = 5;
+            this.btn_negative.Text = "Negative Image";
+            this.btn_negative.UseVisualStyleBackColor = true;
+            this.btn_negative.Click += new System.EventHandler(this.btn_negative_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 519);
+            this.Controls.Add(this.btn_negative);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb_result);
+            this.Controls.Add(this.pb_1);
+            this.Controls.Add(this.btn_Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_result)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Open;
+        private System.Windows.Forms.PictureBox pb_1;
+        private System.Windows.Forms.PictureBox pb_result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_negative;
+    }
+}
+
---
+++ Image Negative/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Negative
+{
+    public partial class Form1 : Form
+    {
+        Image File;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "JPG(*.JPG)|*.jpg";
+            if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                File = Image.FromFile(open.FileName);
+                pb_1.Image = File;
+
+            }
+        }
+
+        private void btn_negative_Click(object sender, EventArgs e)
+        {
+
+            Color p;
+
+            pb_1.Image = File;
+
+            Bitmap bmp1 = new Bitmap(File);
+
+
+            int Width = bmp1.Width;
+            int Height = bmp1.Height;
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    p = bmp1.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+
+
+
+                    int sub1 = 255 - r;
+                    int sub2 = 255 - g;
+                    int sub3 = 255 - b;
+
+
+                    bmp1.SetPixel(x, y, Color.FromArgb(a, sub1, sub2, sub3));
+
+                }
+            }
+           pb_result.Image = bmp1;
+        }
+    }
+}
